706dbfd3de63ab1b78cb6ec55ce922342660167a
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-06-18 21:54:50 +0700

    adding auth

--- src/app/action.js
+++ src/app/action.js
@@ -1,12 +1,74 @@
 "use server";
 
-import { createNote } from "@/services/notes";
-import { revalidatePath } from "next/cache";
+import { prisma } from "@/utils/prisma";
+import bcrypt from "bcrypt";
 
-export async function createTodoAction(formData) {
-  const title = formData.get("title");
-  const content = formData.get("content");
+export async function registerAction(formData) {
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-  await createNote(title, content);
-  revalidatePath("/");
+  console.log({ name, email, password });
+
+  try {
+    await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    const hashedPassword = await bcrypt.hash(password, 12); // 10 - 13 -> Not much resources / Not too long to process.
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+  } catch (err) {
+    console.log(err);
+    return {
+      message: "User already exist",
+    };
+  }
+}
+
+export async function loginAction(formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let user = null;
+
+  try {
+    user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+  } catch (err) {
+    console.log("Something went wrong!");
+    return {
+      message: "Something went wrong!",
+    };
+  }
+
+  if (!user) {
+    console.log("User does not exist");
+    return {
+      message: "User does not exist!",
+    };
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    console.log("Invalid Password!");
+    return {
+      message: "Invalid Password!",
+    };
+  }
+
+  console.log("Login success!");
+  // Authorization
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { createTodoAction } from "./action";
-import { getNotes } from "@/services/notes";
+import { loginAction } from "./action";
 
 export default async function Home() {
-  const notes = await getNotes();
-
   return (
     <div>
-      <form action={createTodoAction}>
-        <Input name="title" />
-        <Textarea name="content" />
-        <Button>Create</Button>
+      <form action={loginAction}>
+        <Input name="email" placeholder="Email" type="email" />
+        <Input name="password" placeholder="Password" type="password" />
+        <Button>Login</Button>
       </form>
-      <h3>My Notes</h3>
-      <div>
-        {notes.map((note) => {
-          return <div key={note.id}>{note.title}</div>;
-        })}
-      </div>
     </div>
   );
 }
